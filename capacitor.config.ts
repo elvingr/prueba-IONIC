9d6de4b2bf8437c54d142a0654d792c9d161231c
Elvin Gonzalez <el_vin024@hotmail.com>
2023-03-07 15:39:05 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Practicas Tecnica LancerGroup',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
